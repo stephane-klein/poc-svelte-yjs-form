5f7b59577d991c5df0ec30a1cfc7adaae4dfd4d8
Stéphane Klein <contact@stephane-klein.info>
2023-07-27 20:13:08 +0200

    First import

---
+++ README.md
@@ -0,0 +1,11 @@
+# SvelteKit Yjs form POC
+
+Context: https://github.com/stephane-klein/backlog/issues/274
+
+## Getting started
+
+```sh
+$ asdf install
+$ pnpm install
+$ pnpm run dev
+```
---
+++ src/routes/+page.svelte
@@ -0,0 +1,15 @@
+<h1>Edit issue</h1>
+
+<form method="POST">
+    <label for="name">Title:</label> <input id="name" name="name" placeholder="John Doe" /><br />
+    <label for="category">Category:</label>
+    <select name="category" id="category">
+        <option value="bug">Bug</option>
+        <option value="feature">Feature</option>
+    </select>
+
+    <label for="description">Description:</label><br />
+    <textarea id="descrption" name="descrption" placeholder="Issue description" /><br />
+
+    <input type="submit" value="Save" />
+</form>
